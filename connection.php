4b07c1555e614bfcb4264b48eade731a6a5f444c
nikhil131313 <nikhilbulchandani1313@gmail.com>
2020-08-25 20:30:28 +0530

    commit

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+    $conn=mysqli_connect('localhost','root',"12345",'examportal');
+    if($conn==false)
+      {
+      	 echo "connection failed";
+
+      }
+      else
+    
+
+
+       ?>
